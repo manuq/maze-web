d84d97bb2477da813dc3d276ef89767269d5e3e0
Lionel Laské <llaske@c2s.fr>
2014-08-01 07:33:03 +0200

    Fix issue on click - sprite only go to the west

--- js/activity.js
+++ js/activity.js
@@ -497,8 +497,8 @@ define(function (require) {
             var px = cellWidth * (player.x + 0.5);
             var py = cellHeight * (player.y + 0.5);
 
-            var x = event.x;
-            var y = event.y;
+            var x = event.clientX;
+            var y = event.clientY;
 
             var canvas = document.getElementById("maze");
             x -= canvas.offsetLeft;
